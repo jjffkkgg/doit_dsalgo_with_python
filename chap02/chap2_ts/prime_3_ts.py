80ccb7014b61f6b958b059d94e80ecacfac1aae7
Taehwan Seo <jjffkkgg@gmail.com>
2020-08-04 22:22:12 -0400

    Day_3

---
+++ chap02/chap2_ts/prime_3_ts.py
@@ -0,0 +1,23 @@
+﻿#%%
+
+counter = 0
+prime = []
+prime.append(2)
+prime.append(3)
+
+max = 1001
+j = 0
+
+for i in range(5,max,2):
+    while (i ** 0.5) >= prime[j]:
+        counter += 2
+        j += 1
+        if i % prime[j] == 0:
+            break
+    else:
+        prime.append(i)
+        j = 0
+#        elif i % j != 0 and j == prime[-1]:
+#           prime.append(i)
+print(f"1부터 {max}까지 소수: {prime}")
+print(f"나눗셈 횟수: {counter}")
---
+++ chap03/chap3_ts/ssearch_while_ts.py
@@ -0,0 +1,23 @@
+﻿#%%
+from typing import Any,Sequence
+#%%
+def sqsearch(lst: Sequence, key: Any) -> int:
+    j = 0
+    while True:
+        if j == len(lst):
+            return -1
+        if key == lst[j]:
+            return j
+        j += 1
+    
+#%%
+if __name__ == "__main__":
+    
+    n = int(input("원소 수 입력: "))
+    x = [None]*n
+    for i in range(n):
+        x[i] = int(input(f"x[{i}]: "))
+
+    f = int(input("검색할 값 입력: "))
+
+    print(f"검색값의 인덱스는 {sqsearch(x,f)}")
