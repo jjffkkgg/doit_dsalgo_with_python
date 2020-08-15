918ce9be3a7c868731d2825d98350bc0a1a35a90
Taehwan Seo <jjffkkgg@gmail.com>
2020-08-14 22:45:30 -0400

    Day 11, 문자열 서칭은 훑고 넘어감(잘 쓰지않음). 한국 가기 전 마지막 커밋.

---
+++ chap07/chap7_ts/bm_ts.py
@@ -0,0 +1,30 @@
+﻿#%%
+def bm_match(txt:str, pat:str) -> int:
+    skip = [None] * 256
+
+    for pt in range(256):
+        skip[pt] = len(pat)
+    for pt in range(len(pat)):
+        skip[ord(pat[pt])] = len(pat) - pt - 1      # 중복되지 않는 C는?
+
+    while pt < len(txt):
+        pp = len(pat) - 1
+        while txt[pt] == pat[pp]:
+            if pp == 0:
+                return pt
+            pt -= 1
+            pp -= 1
+        pt += skip[ord(txt[pt])] if skip[ord(txt[pt])] > len(pat) - pp else len(pat) - pp  # 표만큼 넘어가는데, 그조건이...?
+
+    return -1
+
+if __name__ == "__main__":
+    s1 = input('텍스트: ')
+    s2 = input('패턴: ')
+
+    idx = bm_match(s1,s2)
+
+    if idx == -1:
+        print('검색 결과가 없음')
+    else:
+        print(f'{(idx + 1)} 번째 문자 일치')
